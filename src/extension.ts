9b79721ead0c59fd6e0f3a6f9f9cdfe6d26a073f
AkiEga <akira.ega@gmail.com>
2024-02-24 20:21:36 +0900

    export to touchSvgCmd

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,51 @@ import fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+async function touchSvgFile() {
+	// The code you place here will be executed every time your command is executed
+	// Display a message box to the user
+	let editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+	let ws = vscode.workspace.getWorkspaceFolder(editor?.document.uri);
+	if (!ws) {
+		return;
+	}
 
+	// Touch(=create=write) an drawio.svg file
+	let conf = vscode.workspace.getConfiguration("touch-drawio-svg");
+	// get imgDir default value from package.json
+	let imgDir:string = conf.get("img-dir")??"";
+	let newFileUri = ws.uri;
+	let newFileContent = new Uint8Array();
+	try {
+		// Check if new file is already exist
+		for (let id = 0;; id++) {
+			let newFileName = `temp_${id}.drawio.svg`;
+			newFileUri = vscode.Uri.joinPath(ws.uri, imgDir, newFileName);
+			// let ret = await vscode.workspace.findFiles(newFileUri.fsPath);
+			if (!fs.existsSync(newFileUri.fsPath)) {
+				// passed
+				break;
+			}
+		}
+		vscode.workspace.fs.writeFile(newFileUri, newFileContent);	
+	} catch (error) {
+		console.error("[Error] failed to write a drawio.svg file from touch-drawio-svg extension!")
+	}
+	vscode.window.showInformationMessage(`write new file: ${newFileUri} from touch-drawio-svg!`);
+
+	// append a drawio.svg markdown link to active editor
+	editor.edit((e)=>{
+		if (ws && editor) {
+			let currentPos = editor.selection.end;
+			let parentDir = path.dirname(editor.document.uri.fsPath);
+			let relative_path_from_this_editor_doc = path.relative(parentDir, newFileUri.path);
+			e.insert(currentPos, `![](${relative_path_from_this_editor_doc})`)
+		}
+	})
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -17,49 +61,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('touch-drawio-svg.create-new-drawio-svg', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		let editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		let ws = vscode.workspace.getWorkspaceFolder(editor?.document.uri);
-		if (!ws) {
-			return;
-		}
-
-		// Touch(=create=write) an drawio.svg file
-		let imgDir = "img";
-		let newFileUri = ws.uri;
-		let newFileContent = new Uint8Array();
-		try {
-			// Check if new file is already exist
-			for (let id = 0;; id++) {
-				let newFileName = `temp_${id}.drawio.svg`;
-				newFileUri = vscode.Uri.joinPath(ws.uri, imgDir, newFileName);
-				// let ret = await vscode.workspace.findFiles(newFileUri.fsPath);
-				if (!fs.existsSync(newFileUri.fsPath)) {
-					// passed
-					break;
-				}
-			}
-			vscode.workspace.fs.writeFile(newFileUri, newFileContent);	
-		} catch (error) {
-			console.error("[Error] failed to write a drawio.svg file from touch-drawio-svg extension!")
-		}
-		vscode.window.showInformationMessage(`write new file: ${newFileUri} from touch-drawio-svg!`);
-
-		// append a drawio.svg markdown link to active editor
-		editor.edit((e)=>{
-			if (ws && editor) {
-				let currentPos = editor.selection.end;
-				let parentDir = path.dirname(editor.document.uri.fsPath);
-				let relative_path_from_this_editor_doc = path.relative(parentDir, newFileUri.path);
-				e.insert(currentPos, `![](${relative_path_from_this_editor_doc})`)
-			}
-		})
-	});
+	let disposable = vscode.commands.registerCommand('touch-drawio-svg.create-new-drawio-svg', touchSvgFile);
 
 	context.subscriptions.push(disposable);
 }
